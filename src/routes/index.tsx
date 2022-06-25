30c7670df1845cdfaabb8a16a9f7d6797bdff5e7
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2022-06-25 05:36:12 -0400

    Restore default route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import PageNotFound from '../pages/404'
 import Index from '../pages/home'
 
 export default function AppRoutes() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='*' element={<Index />} />
+				<Route path='*' element={<PageNotFound />} />
+				<Route path='/' element={<Index />} />
 			</Routes>
 		</BrowserRouter>
 	)
