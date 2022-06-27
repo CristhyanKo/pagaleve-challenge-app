11b8ced65ed1f25d0bf918eaf3633c4d69d89cc3
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2022-06-27 02:39:59 -0400

    Add location select in request header

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,5 +1,5 @@
 import api from '../utils/api'
-
+import i18n from '../i18n'
 export default class BaseService {
 	private _routeBase: string
 
@@ -8,32 +8,56 @@ export default class BaseService {
 	}
 
 	async store(data: object) {
-		const response = await api.post(`${this._routeBase}/store`, data)
+		const response = await api.post(`${this._routeBase}/store`, data, {
+			headers: {
+				location: i18n.language,
+			},
+		})
 		return response.data.result.data
 	}
 
 	async update(id: string, data: object) {
-		const response = await api.post(`${this._routeBase}/update/${id}`, data)
+		const response = await api.post(`${this._routeBase}/update/${id}`, data, {
+			headers: {
+				location: i18n.language,
+			},
+		})
 		return response.data.result.data
 	}
 
 	async delete(id: string) {
-		const response = await api.delete(`${this._routeBase}/delete/${id}`)
+		const response = await api.delete(`${this._routeBase}/delete/${id}`, {
+			headers: {
+				location: i18n.language,
+			},
+		})
 		return response.data.result.data
 	}
 
 	async getOne(data: object) {
-		const response = await api.post(`${this._routeBase}/get`, data)
+		const response = await api.post(`${this._routeBase}/get`, data, {
+			headers: {
+				location: i18n.language,
+			},
+		})
 		return response.data.result.data
 	}
 
 	async getAll() {
-		const response = await api.get(`${this._routeBase}/getAll`)
+		const response = await api.get(`${this._routeBase}/getAll`, {
+			headers: {
+				location: i18n.language,
+			},
+		})
 		return response.data.result.data
 	}
 
 	async getAllWithPaginate(page: number, limit: number) {
-		const response = await api.get(`${this._routeBase}/getAllWithPaginate?page=${page}&limit=${limit}`)
+		const response = await api.get(`${this._routeBase}/getAllWithPaginate?page=${page}&limit=${limit}`, {
+			headers: {
+				location: i18n.language,
+			},
+		})
 		return response.data.result.data
 	}
 }
